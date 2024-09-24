18818a3f361ca94020719b8609292d5cb4ea1043
syafiraazzahra24 <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2024-09-24 08:31:09 +0700

    Create formdesign.php

---
+++ formdesign.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pemilihan Mata Kuliah</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #ffecb3; /* Warna latar kuning muda */
+        }
+    </style>
+</head>
