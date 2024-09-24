49e0d9cc079e584fc4cc37fe9df617f5e99434a5
syafiraazzahra24 <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2024-09-24 23:20:12 +0700

    bootstrap
    menambhkan bootstrap

--- halamanutama.php
+++ halamanutama.php
@@ -6,6 +6,8 @@
     <title>Halaman Utama KRS</title>
 </head>
 <body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
     
 </body>
 </html>
