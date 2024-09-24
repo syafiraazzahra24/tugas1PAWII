23ccd1b652533e411d5e924b3bf53b2590b54ee5
syafiraazzahra24 <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2024-09-24 23:21:20 +0700

    warna
    menambahkn warna sesuai dengan kebutuhan

--- halamanutama.php
+++ halamanutama.php
@@ -7,6 +7,13 @@
 </head>
 <body>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+<style>
+        body {
+            height: 100%;
+            margin: 0;
+            background-color: #E6E6FA; 
+        }
+    </style>
 
     
 </body>
