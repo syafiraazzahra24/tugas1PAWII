f27dcef67c4115c913f18720feb1fdaf3876a1d8
Erikaa311 <160854903+Erikaa311@users.noreply.github.com>
2024-09-24 22:57:29 +0700

    form registrasi
    menambahkan background

---
+++ halaman_registrasi.php
@@ -0,0 +1,25 @@
+<form action="form_design.php" method="post">
+<form action="halaman_detail.php" method="post">
+    
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Registrasi KRS</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Custom CSS -->
+    <style>
+        body {
+            background-color: #f1f8e9; /* Warna latar hijau muda */
+        }
+    </style>
+    </head>
+<body>
+   
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
