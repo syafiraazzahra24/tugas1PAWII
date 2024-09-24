73165c5878bc88fbad3b137dcd190d49d6baf67d
syafiraazzahra24 <syafiraazzahra_2226240082@mhs.mdp.ac.id>
2024-09-24 08:40:50 +0700

    Update halamanutama.php

--- halamanutama.php
+++ halamanutama.php
@@ -3,7 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Halaman Utama KRS</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    
 </head>
 <body>
     
