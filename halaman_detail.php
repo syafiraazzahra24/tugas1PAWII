07cf37e3852f75d2ca2d41f75b96f15c45453c4b
Erikaa311 <160854903+Erikaa311@users.noreply.github.com>
2024-09-24 09:45:35 +0700

    update hal detail
    menambahkan dan menggunakanboostrap

--- halaman_detail.php
+++ halaman_detail.php
@@ -169,6 +169,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </div>
     </div>
     
-   
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <script>
+        // Initialize all tooltips
+        var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+        var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+            return new bootstrap.Tooltip(tooltipTriggerEl)
+        });
+    </script>
 </body>
 </html>
